1d7c6481081179f1617bf298e95d056b13bd9bb3
Igor <vorobiov.777@yandex.ru>
2022-09-23 15:00:41 +0300

    Added TimeMessageHandler

--- AspCoreMetanit/Program.cs
+++ AspCoreMetanit/Program.cs
@@ -3,13 +3,15 @@ using AspCoreMetanit;
 var builder = WebApplication.CreateBuilder();
  
 var services = builder.Services;
-services.AddTransient<ITimeService, ShortTimeService>();
+services.AddTransient<ITimeService, LongTimeService>();
 services.AddTransient<TimeMessage>();
 
 // services.AddTransient<ITimeService, ShortTimeService>();
 //services.AddTransient<TimeService>();
 
 var app = builder.Build();
+
+app.UseMiddleware<TimeMessageMiddleware>();
 
 app.Run(async context =>
 {
---
+++ AspCoreMetanit/TimeMessageMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace AspCoreMetanit;
+
+public class TimeMessageMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TimeMessageMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, ITimeService timeService)
+    {
+        context.Response.ContentType = "text/html; charset=utf-8";
+        await context.Response.WriteAsync(
+            $"Current time of {TypeDescriptor.GetClassName(timeService)}\n{timeService.GetTime()}");
+    }
+}
